37697025f79e458bf612794aa18bab89d04b12e4
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2022-09-08 15:26:21 +0900

    feat:Add Assis request menu

--- client/src/components/organisms/HomeLayout.tsx
+++ client/src/components/organisms/HomeLayout.tsx
@@ -1,6 +1,17 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import {
+  accessTokenState,
+  handleHelpSideBar,
+} from '~/others/store';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { shadowCssForMUI } from '~/others/cssLibrary';
+import Button from '@mui/material/Button';
+import ArrowBack from '@mui/icons-material/ArrowBack';
+import ArrowForward from '@mui/icons-material/ArrowForward';
 
 const StyledBody = styled.div`
     height: 100vh;
@@ -39,7 +50,19 @@ const StyledDown = styled.img`
     }
 `;
 
+const StyledMenu = styled.img`
+  
+    width: 420px;
+    background-position: 10% 100px;
+    position: fixed;
+    left: 0px;
+    bottom: 50px;
+    }
+`;
+
+
 const Home = () => {
+  const [anchorElHelpCall, setAnchorElHelpCall] = React.useState<null | HTMLElement>(null);
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -48,12 +71,60 @@ const Home = () => {
     });
   };
 
+  const handleOpenHelpCallModal = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElHelpCall(event.currentTarget);
+  };
+
+  const handleCloseHelpCallModal = () => {
+    setAnchorElHelpCall(null);
+  };
+
   return (
     <StyledBody>
       <StyledIllust src='img/homeIllust.png' />
-      <IconButton sx={{ p: 0 }}>
+      <IconButton onClick={handleOpenHelpCallModal} sx={{ p: 0 }}>
         <StyledImg src='/img/warning.png' />
       </IconButton>
+    
+      <Menu
+          open={Boolean(anchorElHelpCall)}
+          onClose={handleCloseHelpCallModal}
+          sx={{ mt: '10px', '& ul': {padding: 0,},}}>
+        <Box sx={{ position: 'fixed', right: '40px', bottom: '170px', width: '350px', height: '150px', ...shadowCssForMUI }}>
+            <Typography
+                sx={{
+                        fontSize: '18px',
+                        lineHeight: '28px',
+                        height: '55px',
+                        alignItems: 'center',
+                        paddingTop: '20px',
+                        textAlign:'center',
+
+                      }}>
+                도움 요청 시 라인 내 입주민에게
+            </Typography>
+            <Typography
+                sx={{    fontSize: '18px', textAlign:'center', height: '40px',}}>
+                도움 요청을 알립니다.
+            </Typography>
+            
+            <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
+                <Button
+                    variant='outlined'
+                    color='inherit'
+                    sx={{ height: '32px' }}
+                    startIcon={<ArrowBack />}
+                  >돌아가기</Button>
+                <Button
+                    variant='contained'
+                    color='error'
+                    sx={{ height: '32px' }}
+                    endIcon={<ArrowForward />}
+                  >동의 후 도움 요청</Button>
+            </Box>
+                  </Box>
+      </Menu>
+
       <StyledDown src='/img/down.png' />
     </StyledBody>
   );
